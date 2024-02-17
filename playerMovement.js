c6784d015356f3d2111ea1234d1bce904199ad15
Varnesh1 <100467880+Varnesh1@users.noreply.github.com>
2024-02-17 16:52:02 +0000

    Add files via upload

---
+++ playerMovement.js
@@ -0,0 +1,20 @@
+AFRAME.registerComponent("player-movement", {
+    init: function () {
+      this.walk();
+    },
+    walk: function () {
+      window.addEventListener("keydown", (e) => {
+        // Add the condition to play sound
+        
+        var entity = document.querySelector("#sound2");
+        entity.components.sound.playSound()
+       
+
+
+
+      });
+    },
+  });
+
+
+  
